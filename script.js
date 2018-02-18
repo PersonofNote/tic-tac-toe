29ff9d314c1935058454e413ce005e2d533ac877
Jessica L.T. Martin <apersonofnote@gmail.com>
2018-02-17 19:41:18 -0800

    Realized that arbitrary data is stored as a string, therefore even if an element's dataset is true, the if-statement that depends on that will not run.

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
 const gameBoard = (() => {
 	const spots = [".", ".",".",".",".",".",".",".","."];
-var playerOne = new player('Joe', 'X', true);
-var playerTwo = new player('Susan', 'O', false);
 var gameController;
 
-/*const spots = [
-{name: "1", value: "X"},
-{name: "2", value: "O"},
-{name: "3", value: " X"},
-{name: "4", value: ""},
-{name: "5", value: "x"},
-{name: "6", value: "o"},
-{name: "7", value: "x"},
-{name: "8", value: "o"},
-{name: "9", value: "x"},
-];
-*/
 for (var i = 0; i < spots.length; i++) {
     var newSpot = document.createElement('div');
     newSpot.id = i; 
     newSpot.className = "spot";
+    newSpot.dataset.isClear = 'true';
     //newSpot.innerHTML = spots[i].value;
     newSpot.innerHTML = spots[i];
-  
     document.getElementById("game-container").appendChild(newSpot);
      newSpot.addEventListener('click', function() { var clickId = this.id; play(clickId); return clickId; } )
     console.log(newSpot.id);
@@ -37,11 +23,42 @@ function player(name,mark,isTurn) {
 	this.isTurn = isTurn;
 }
 
+var playerOne = new player('Joe', 'X', true);
+var playerTwo = new player('Susan', 'O', false);
 
 
+console.log(playerOne);
 
 function play(clicked) {
-	document.getElementById(clicked).innerHTML = "X";
+	var currentSpot = document.getElementById(clicked);
+	var toMark;
+	console.log(currentSpot.dataset.isClear);
+	if (currentSpot.dataset.isClear == 'true') { console.log("I'm true!");
+	if (playerOne.isTurn == true) {
+		toMark = "X";
+		document.getElementById("turnDisplay".innerHTML = "Player Two's Turn.")
+		playerOne.isTurn = false;
+		playerTwo.isTurn = true;
+		
+	}
+	else if (playerTwo.isTurn == true) {
+		toMark = "O";
+		playerTwo.isTurn = false;
+		playerOne.isTurn = true;
+	}
+	currentSpot.innerHTML = toMark;
+	currentSpot.dataset.isClear = 'false';
+	console.log(currentSpot.dataset)
+	//console.log("Spot " + currentSpot.id + "is now full.");
+	//console.log("Because isClear = " + currentSpot.dataset.isClear);
+	
+	}
+	else if (currentSpot.dataset.isClear == 'false') {
+		console.log("This spot is full.");
+		return;
+	}
+	
+	
 }
 
 /*
