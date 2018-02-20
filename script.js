e5fa3461952f251592444c37856c01b088a4561a
PersonofNote <jtm89@nau.edu>
2018-02-20 08:40:07 -0800

    Needs a refactor; functions and objects are a bit jumbled, as I started this project a good while ago, before any meetup lectures. Committing as a record of process and a comparison.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const gameBoard = (() => {
-	const spots = [".", ".",".",".",".",".",".",".","."];
+var spots = [];
 var gameController;
+/* Where I'm at: I need to loop through the array but that means I have to push stuff to the array to begin with.
+alkdghlkaghlkasgd
+*/
 
-for (var i = 0; i < spots.length; i++) {
+for (var i = 0; i < 8; i++) {
     var newSpot = document.createElement('div');
     newSpot.id = i; 
     newSpot.className = "spot";
@@ -10,11 +13,13 @@ for (var i = 0; i < spots.length; i++) {
     //newSpot.innerHTML = spots[i].value;
     newSpot.innerHTML = spots[i];
     document.getElementById("game-container").appendChild(newSpot);
-     newSpot.addEventListener('click', function() { var clickId = this.id; play(clickId); return clickId; } )
+     newSpot.addEventListener('click', function() { var clickId = this.id; pickSpot(clickId); return clickId; } )
     console.log(newSpot.id);
+   
     //console.log(newSpot.value);
 } 
 return spots;
+console.log(spots);
 })();
 
 function player(name,mark,isTurn) {
@@ -27,38 +32,39 @@ var playerOne = new player('Joe', 'X', true);
 var playerTwo = new player('Susan', 'O', false);
 
 
-console.log(playerOne);
 
-function play(clicked) {
+function pickSpot(clicked) {
 	var currentSpot = document.getElementById(clicked);
 	var toMark;
-	console.log(currentSpot.dataset.isClear);
-	if (currentSpot.dataset.isClear == 'true') { console.log("I'm true!");
+	if (currentSpot.dataset.isClear == 'true') {
 	if (playerOne.isTurn == true) {
 		toMark = "X";
 		document.getElementById("turnDisplay".innerHTML = "Player Two's Turn.")
 		playerOne.isTurn = false;
 		playerTwo.isTurn = true;
-		
 	}
 	else if (playerTwo.isTurn == true) {
 		toMark = "O";
+		document.getElementById("turnDisplay".innerHTML = "Player One's Turn.")
 		playerTwo.isTurn = false;
 		playerOne.isTurn = true;
 	}
 	currentSpot.innerHTML = toMark;
 	currentSpot.dataset.isClear = 'false';
-	console.log(currentSpot.dataset)
-	//console.log("Spot " + currentSpot.id + "is now full.");
-	//console.log("Because isClear = " + currentSpot.dataset.isClear);
-	
+	console.log(gameBoard);
 	}
 	else if (currentSpot.dataset.isClear == 'false') {
-		console.log("This spot is full.");
 		return;
 	}
-	
-	
+	}
+
+function newGame() {
+for (var i = 0; i < spots.length; i++)
+{
+	var spot = document.getElementById(i);
+	spot.innerHTML = " ";
+	spot.dataset.isClear = "true";
+}
 }
 
 /*
